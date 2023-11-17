9fc30e13c7eeee8f48b5755b73ab4e4aeff3024d
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2023-11-18 00:40:29 +1100

    Complete Ch 12 - Mutating Data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,7 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+// Use Zod to update the expected types
 const InvoiceSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -12,6 +13,8 @@ const InvoiceSchema = z.object({
   status: z.enum(["pending", "paid"]),
   date: z.string(),
 });
+
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 
@@ -28,6 +31,25 @@ export async function createInvoice(formData: FormData) {
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
